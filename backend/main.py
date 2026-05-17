b6c9b9a9854d52a6e6a2bce17ec54a1ebe8eae0a
kishore-stack <s.kishorekumar.mech@gmail.com>
2026-05-17 22:45:26 +0530

    fix cors for vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -21,9 +21,9 @@ app = FastAPI(
 # CORS
 origins = [
     "http://localhost:5173",
+    "https://invoice-iq-sd73.vercel.app",
     "https://invoice-iq-sd73-2rj8cjwiz-kishores-projects-6fd240ae.vercel.app"
 ]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
